e8450397788174ba321d84a571ade9d49e1ad9eb
Kendal Sosa <kendal.alfonso37@gmail.com>
2018-11-19 15:33:16 -0600

    Buzzer para Arduino, beep cuando detecte tarjeta

--- Arduino/sketch_UIDRead/sketch_UIDRead.ino
+++ Arduino/sketch_UIDRead/sketch_UIDRead.ino
@@ -1,15 +1,42 @@
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * ARC Sistema de busqueda de pacientes por tarjetas RFID
+ * 
+ * Objetivo: buscar identificadores de paciente a traves de una tarjeta RFID con Arduino Uno
+ * al detectar una tarjeta RFID debera recorrer los bytes recuperados del identificador y 
+ * hacer un pitido con un buzzer
+ * --------------------------------------------------------------------------------------------------------------------
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ */
+
+// Librerias para Modulo RFID
 #include <SPI.h>
 #include <MFRC522.h>
-
+// Configuracion de pines para RFID
 #define RST_PIN  9
 #define SS_PIN  10
-
+// Pin para Buzzer
+int buzzPin = 3;
+// Inicializacion para Modulo RFID
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup(){
+  // Seteando Modulo RFID
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
+  // Seteando Buzzer
+  pinMode(buzzPin, OUTPUT);
 }
 
 void loop(){
@@ -17,7 +44,9 @@ void loop(){
     return;
   }
 
+
   if( ! mfrc522.PICC_ReadCardSerial() ){
+    
     return;
   }
 
@@ -32,5 +61,10 @@ void loop(){
     Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
   Serial.println();
+  // Pitido del Buzzer
+  digitalWrite(buzzPin, HIGH);
+  delay(200);
+  digitalWrite(buzzPin, LOW);
+  delay(200);
   mfrc522.PICC_HaltA();
 }
